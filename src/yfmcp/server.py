bd35202d59c4a3657197e76cce22f48e9e6f654f
narumi <toucans-cutouts0f@icloud.com>
2025-03-30 19:49:41 +0800

    fix lint error

--- src/yfmcp/server.py
+++ src/yfmcp/server.py
@@ -1,24 +1,25 @@
 from typing import Annotated
 
+import yfinance as yf
 from mcp.server.fastmcp import FastMCP
 from pydantic import Field
-import yfinance as yf
 
 # https://github.com/jlowin/fastmcp/issues/81#issuecomment-2714245145
 mcp = FastMCP("Yahoo Finance MCP Server", log_level="ERROR")
 
 
-
 @mcp.tool()
 def get_ticker_info(symbol: Annotated[str, Field(description="The stock symbol")]) -> str:
-    ticker =yf.Ticker(symbol)
+    ticker = yf.Ticker(symbol)
     return str(ticker.info)
-    
+
+
 @mcp.tool()
 def get_ticker_news(symbol: Annotated[str, Field(description="The stock symbol")]) -> str:
     ticker = yf.Ticker(symbol)
     news = ticker.get_news()
     return str(news)
-    
+
+
 def main():
     mcp.run()
