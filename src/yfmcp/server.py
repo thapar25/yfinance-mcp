ea7063dbd8a5c955ca9261f1cc88db536a5f2aa9
narumi <toucans-cutouts0f@icloud.com>
2025-03-30 20:50:15 +0800

    ✨ feat(server): add functions for sector and industry retrieval

--- src/yfmcp/server.py
+++ src/yfmcp/server.py
@@ -3,6 +3,7 @@ from typing import Annotated
 import yfinance as yf
 from mcp.server.fastmcp import FastMCP
 from pydantic import Field
+from yfinance.const import SECTOR_INDUSTY_MAPPING
 
 from .types import Market
 
@@ -48,6 +49,50 @@ def get_market(
 ) -> str:
     m = yf.Market(market.value)
     return str(m.status) + "\n" + str(m.summary)
+
+
+@mcp.tool()
+def get_sector_industy_mapping() -> str:
+    lines = []
+    for k, v in SECTOR_INDUSTY_MAPPING.items():
+        lines.append(f"{k}: {v}")
+    return "\n".join(lines)
+
+
+@mcp.tool()
+def get_sector(
+    sector: Annotated[
+        str, Field(description="The sector to get, use get_sector_industy_mapping() to get available sectors.")
+    ],
+) -> str:
+    s = yf.Sector(sector)
+    return "\n\n".join(
+        [
+            f"<overview>\n{s.overview}\n</overview>",
+            f"<top_companies>\n{s.top_companies}\n</top_companies>",
+            f"<top_etfs>\n{s.top_etfs}\n</top_etfs>",
+            f"<top_mutual_funds>\n{s.top_mutual_funds}\n</top_mutual_funds>",
+            f"<research_reports>\n{s.research_reports}\n</research_reports>",
+        ]
+    )
+
+
+@mcp.tool()
+def get_industry(
+    industry: Annotated[
+        str, Field(description="The industry to get, use get_sector_industy_mapping() to get available industries.")
+    ],
+) -> str:
+    i = yf.Industry(industry)
+    return "\n\n".join(
+        [
+            f"<overview>\n{i.overview}\n</overview>",
+            f"<top_growth_companies>\n{i.top_growth_companies}\n</top_growth_companies>",
+            f"<top_companies>\n{i.top_companies}\n</top_companies>",
+            f"<top_performing_companies>\n{i.top_performing_companies}\n</top_performing_companies>",
+            f"<research_reports>\n{i.research_reports}\n</research_reports>",
+        ]
+    )
 
 
 def main():
