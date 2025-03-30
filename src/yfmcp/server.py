18c839fcb21f26f01b938439a521aaccc8ab55c4
narumi <toucans-cutouts0f@icloud.com>
2025-03-30 19:47:14 +0800

    ✨ feat(server): add functions to retrieve ticker info and news

--- src/yfmcp/server.py
+++ src/yfmcp/server.py
@@ -2,19 +2,23 @@ from typing import Annotated
 
 from mcp.server.fastmcp import FastMCP
 from pydantic import Field
+import yfinance as yf
 
 # https://github.com/jlowin/fastmcp/issues/81#issuecomment-2714245145
 mcp = FastMCP("Yahoo Finance MCP Server", log_level="ERROR")
 
 
+
 @mcp.tool()
-def add_numbers(
-    a: Annotated[float, Field(description="The first number")],
-    b: Annotated[float, Field(description="The second number")],
-) -> str:
-    """Add two numbers and return the result as a string."""
-    return f"{a} + {b} = {a+b} from MCP Template"
-
-
+def get_ticker_info(symbol: Annotated[str, Field(description="The stock symbol")]) -> str:
+    ticker =yf.Ticker(symbol)
+    return str(ticker.info)
+    
+@mcp.tool()
+def get_ticker_news(symbol: Annotated[str, Field(description="The stock symbol")]) -> str:
+    ticker = yf.Ticker(symbol)
+    news = ticker.get_news()
+    return str(news)
+    
 def main():
     mcp.run()
